f0e969c79330d425ffef4e1cee95d73953ce1398
g0vn0c0d3r <leonovich.mikhail@gmail.com>
2020-03-10 10:59:39 +0300

    upload

--- offer.py
+++ offer.py
@@ -3,31 +3,22 @@ from datetime import date
 
 
 class Offer:
-	def __init__(self, *, offer_id, date_from='2020-02-15', date_to=date.today()):
+	def __init__(self, *, offer_id):
 		self.offer_id = offer_id
 		self._api_url = 'https://api-lime-finance.affise.com/'
 		self._api_key = 'c666e444eabc1706574ec7973ae4e677'
-		self._raw_data = requests.get(
-			self._api_url + '3.0/stats/custom',
-			headers={'API-Key': self._api_key},
-			params=(
-				('slice[]', ['sub3', 'affiliate', 'year', 'month', 'day']),
-				('filter[date_from]', date_from),
-				('filter[date_to]', date_to),
-				('filter[offer]', self.offer_id)
-				)
-			).json()
 
-	def get_partner_list(self):
-		partners = []
-		for i in range(len(self._raw_data['stats'])):
-			if self._raw_data['stats'][i]['slice']['affiliate']['id'] not in partners:
-				partners.append(self._raw_data['stats'][i]['slice']['affiliate']['id'])
-		return sorted(partners)
-
-	def get_webmasters_list(self):
-		webmasters = []
-		for i in range(len(self._raw_data['stats'])):
-			if (self._raw_data['stats'][i]['slice']['sub3']).isdigit() is True and int(self._raw_data['stats'][i]['slice']['sub3']) not in webmasters:
-				webmasters.append(int(self._raw_data['stats'][i]['slice']['sub3']))
-		return sorted(webmasters)
+	def get_partner_list(self, *, start_date: str, end_date: str):
+		response = requests.get(self._api_url + '3.0/stats/getbypartner',
+								headers={'API-Key': self._api_key},
+								params=(
+									('filter[date_from]', start_date),
+									('filter[date_to]', end_date),
+									('filter[offer]', self.offer_id)
+									)
+								).json()
+		result = []
+		for i in range(len(response['stats'])):
+			if type(response['stats'][i]['slice']['affiliate']) is dict:
+				result.append(response['stats'][i]['slice']['affiliate']['id'])
+		return result
