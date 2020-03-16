898fff07b1bf72beda07a9fcf3bce8f26412e375
leonovichmikhail <leonovich.mikhail@gmail.com>
2020-03-16 12:50:06 +0300

    update

--- custom_clicks.py
+++ custom_clicks.py
@@ -4,7 +4,7 @@ api_url = 'https://api-lime-finance.affise.com/'
 api_key = 'c666e444eabc1706574ec7973ae4e677'
 
 
-r = requests.get(
+clicks = requests.get(
 	api_url + '3.0/stats/custom',
 	headers={'API-Key': api_key},
 	params=(
@@ -12,8 +12,5 @@ r = requests.get(
 		('filter[date_from]', '2020-02-15'),
 		('filter[date_to]', '2020-03-16'),
 		('filter[offer]', 15),
-		('limit', 1)
-		)
-	).json()
-
-print(r)
+	)
+).json()
