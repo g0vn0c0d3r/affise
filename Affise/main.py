b758d9af675211ade1f08e8e429f8ad00d53bf8e
leonovichmikhail <leonovich.mikhail@gmail.com>
2020-05-12 18:12:44 +0300

    update

--- Affise/main.py
+++ Affise/main.py
@@ -9,7 +9,6 @@ konga = Offer(offer_id=OfferId.konga.value)
 lm = lime.get_sverka(date_from='2020-05-01', date_to='2020-05-30')
 lm.sort_values(by='All', ascending=False, inplace=True)
 print(lm)
-# print()
-# kg = konga.get_sverka(date_from='2020-05-01', date_to='2020-05-30')
-# print(kg)
+
+
 
