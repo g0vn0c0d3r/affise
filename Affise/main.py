43ff191b3aae2493f78763b3dcd64bcd78f3f2d9
g0vn0c0d3r <leonovich.mikhail@gmail.com>
2020-05-13 11:28:24 +0300

    offer

--- Affise/main.py
+++ Affise/main.py
@@ -6,6 +6,6 @@ from Affise.Constants import ConversionStatus
 lime = Offer(offer_id=OfferId.lime.value)
 konga = Offer(offer_id=OfferId.konga.value)
 
-lm = lime.get_csv_for_all_partners(date_from='2020-04-01', date_to='2020-04-30')
-print(lm)
+lime.get_csv_for_all_partners(date_from='2020-04-01', date_to='2020-04-30')
+
 
