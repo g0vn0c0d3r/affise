340d5019893b6f94a82174a79352e893cbdba5eb
g0vn0c0d3r <leonovich.mikhail@gmail.com>
2020-05-16 20:24:41 +0300

    offer

--- Affise/main.py
+++ Affise/main.py
@@ -6,7 +6,7 @@ from Affise.Constants import ConversionStatus
 lime = Offer(offer_id=OfferId.lime.value)
 konga = Offer(offer_id=OfferId.konga.value)
 
-lime.get_csv_reports(date_from='2020-04-01', date_to='2020-04-30')
-# print(lime.get_pivot_sverka(date_from='2020-05-01', date_to='2020-05-30'))
+# lime.get_csv_reports(date_from='2020-04-01', date_to='2020-04-30')
+print(lime.get_pivot_sverka(date_from='2020-05-01', date_to='2020-05-30'))
 # print(lime.api_conversions_request(date_from='2020-04-01', date_to='2020-04-30', status=1, limit=10, page=1))
 
