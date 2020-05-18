9487453c4fa6d1798af0a8ed7ef43f7ab603a024
g0vn0c0d3r <leonovich.mikhail@gmail.com>
2020-05-18 21:12:25 +0300

    offer

--- Affise/main.py
+++ Affise/main.py
@@ -7,8 +7,10 @@ import pandas as pd
 lime = Offer(offer_id=OfferId.lime.value)
 konga = Offer(offer_id=OfferId.konga.value)
 
+# print(lime.get_aggregated_monthly_stats(date_from='2020-04-01', date_to='2020-04-31'))
+# print()
+# print(konga.get_aggregated_monthly_stats(date_from='2020-04-01', date_to='2020-04-31'))
 
-# result = lime.get_aggregated_monthly_stats(date_from='2020-05-01', date_to='2020-05-30')
-
+# print()
 result = lime.get_daily_stats(date_from='2020-05-01', date_to='2020-05-30')
 print(result.iloc[:, 0:7])
