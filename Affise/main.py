09d43ebdd6852b2325e5f28f8ff8b809c361161a
leonovichmikhail <leonovich.mikhail@gmail.com>
2020-06-09 20:38:15 +0300

    1

--- Affise/main.py
+++ Affise/main.py
@@ -11,9 +11,9 @@ partners_report = get_partners_daily_stats(offer_id=lime_id, date_from=date_from
 print(partners_report)
 print()
 
-monthly_rep = get_aggregated_affiliate_stats(offer_id=lime_id, date_from=date_from, date_to=date_from)
+monthly_rep = get_aggregated_affiliate_stats(offer_id=lime_id, date_from=date_from, date_to=date_to)
 rep1 = monthly_rep[['raw_clicks', 'conversions', 'loans', 'CR%', 'AR%']].sort_values(by='loans', ascending=False)
-rep2 = monthly_rep[['conversions', 'loans', 'cost', 'CPL', 'CPA']].sort_values(by='loans', ascending=False)
+rep2 = monthly_rep[['conversions', 'loans', 'cost', 'CPL', 'CPA', 'EPC']].sort_values(by='loans', ascending=False)
 print(rep1)
 print()
 print(rep2)
