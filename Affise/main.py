cfe8f8a3c2fcf02dedaa962760f0658508f81172
leonovichmikhail <leonovich.mikhail@gmail.com>
2020-06-10 16:34:06 +0300

    1

--- Affise/main.py
+++ Affise/main.py
@@ -4,8 +4,8 @@ from Affise.final import *
 lime_id = 7
 konga_id = 8
 
-date_from = '2020-06-01'
-date_to = '2020-06-31'
+date_from = '2020-05-09'
+date_to = '2020-06-09'
 
 # partners_report = get_partners_daily_stats(offer_id=lime_id, date_from=date_from, date_to=date_to)
 # print(partners_report)
@@ -18,18 +18,20 @@ date_to = '2020-06-31'
 # print()
 # print(rep2)
 # print()
-
-
+#
+#
 leadgid = get_webmasters_report(offer_id=lime_id, partner_id=30, date_from=date_from, date_to=date_to)
 guruleads = get_webmasters_report(offer_id=lime_id, partner_id=34, date_from=date_from, date_to=date_to)
 leadssu = get_webmasters_report(offer_id=lime_id, partner_id=29, date_from=date_from, date_to=date_to)
-# leadgid.to_csv('leadgid.csv')
-# guruleads.to_csv('guruleads.csv')
-# leadssu.to_csv('leadssu.csv')
-print(leadgid)
-print()
-print(leadssu)
-print()
-print(guruleads)
+
+
+leadgid.to_csv('leadgid.csv')
+guruleads.to_csv('guruleads.csv')
+leadssu.to_csv('leadssu.csv')
+
+# print()
+# print(leadssu)
+# print()
+# print(guruleads)
 
 
