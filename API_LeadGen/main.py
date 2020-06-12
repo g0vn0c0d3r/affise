1c9d58a27bcaac7055a46fde7a1a6ef2810d194b
leonovichmikhail <leonovich.mikhail@gmail.com>
2020-06-12 11:00:56 +0300

    1

--- API_LeadGen/main.py
+++ API_LeadGen/main.py
@@ -2,7 +2,7 @@ import requests
 import time
 
 
-file = open('LIME RU. Blocked 09.06.20.csv').readlines()
+file = open('LIME RU. Blocked 10.06.20.csv').readlines()
 TOKEN = 'zbT1YLJGsEngsYAwI6a5iyx7Bz3qcktPQSy38Sp47zjKfdlJMDlrxn5B34ZfGOiG'
 
 check_list = []
--- Affise/main.py
+++ Affise/main.py
@@ -4,34 +4,27 @@ from Affise.final import *
 lime_id = 7
 konga_id = 8
 
-date_from = '2020-05-09'
-date_to = '2020-06-09'
+date_from = '2020-06-01'
+date_to = '2020-06-31'
 
-# partners_report = get_partners_daily_stats(offer_id=lime_id, date_from=date_from, date_to=date_to)
-# print(partners_report)
-# print()
+partners_report = get_partners_daily_stats(offer_id=lime_id, date_from=date_from, date_to=date_to)
+print(partners_report)
+print()
+
+monthly_rep = get_aggregated_affiliate_stats(offer_id=lime_id, date_from=date_from, date_to=date_to)
+rep1 = monthly_rep[['raw_clicks', 'conversions', 'loans', 'CR%', 'AR%']].sort_values(by='loans', ascending=False)
+rep2 = monthly_rep[['conversions', 'loans', 'cost', 'CPL', 'CPA', 'EPC']].sort_values(by='loans', ascending=False)
+print(rep1)
+print()
+print(rep2)
+print()
 #
-# monthly_rep = get_aggregated_affiliate_stats(offer_id=lime_id, date_from=date_from, date_to=date_to)
-# rep1 = monthly_rep[['raw_clicks', 'conversions', 'loans', 'CR%', 'AR%']].sort_values(by='loans', ascending=False)
-# rep2 = monthly_rep[['conversions', 'loans', 'cost', 'CPL', 'CPA', 'EPC']].sort_values(by='loans', ascending=False)
-# print(rep1)
-# print()
-# print(rep2)
-# print()
+
+# leadgid_june = get_webmasters_report(offer_id=lime_id, partner_id=30, date_from='2020-06-01', date_to='2020-06-31')
+# guruleads_june = get_webmasters_report(offer_id=lime_id, partner_id=34, date_from='2020-06-01', date_to='2020-06-31')
+# leads_june = get_webmasters_report(offer_id=lime_id, partner_id=29, date_from='2020-05-01', date_to='2020-06-31')
 #
-#
-leadgid = get_webmasters_report(offer_id=lime_id, partner_id=30, date_from=date_from, date_to=date_to)
-guruleads = get_webmasters_report(offer_id=lime_id, partner_id=34, date_from=date_from, date_to=date_to)
-leadssu = get_webmasters_report(offer_id=lime_id, partner_id=29, date_from=date_from, date_to=date_to)
-
-
-leadgid.to_csv('leadgid.csv')
-guruleads.to_csv('guruleads.csv')
-leadssu.to_csv('leadssu.csv')
-
-# print()
-# print(leadssu)
-# print()
-# print(guruleads)
-
-
+# leads_june.to_csv('leads.csv')
+# leadgid_june.to_csv('leadgid_june.csv')
+# guruleads_june.to_csv('guruleads_june.csv')
+# leads_june.to_csv('leads_june.csv')
