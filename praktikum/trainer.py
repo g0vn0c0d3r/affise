a305dd09c88bb6a968d9e85436d691653ddcdb7f
leonovichmikhail <leonovich.mikhail@gmail.com>
2020-07-18 13:03:28 +0300

    1

--- praktikum/trainer.py
+++ praktikum/trainer.py
@@ -6,53 +6,54 @@ m = Mystem()
 
 client_data = pd.read_csv('data.csv')
 
+
 # Обработка пропусков
 for employee in client_data['income_type'].unique():
     median_income = client_data[client_data['income_type'] == employee]['total_income'].median()
     client_data.loc[(client_data['income_type'] == employee) & (client_data['total_income'].isnull()), 'total_income'] = median_income
-print(client_data.info())
 
-# columns = client_data.columns
+columns = client_data.columns
+
+for column in columns:
+    if client_data.dtypes[column] == 'object':
+        client_data[column] = client_data[column].str.lower()
+
+# Смена типов данных
+client_data['total_income'] = client_data['total_income'].astype(int)
+
+# Удаление дубликатов
+client_data = client_data.drop_duplicates().reset_index(drop=True)
+
+
+def get_lemma(row: str):
+    lemma = m.lemmatize(row)[-2]
+    return lemma
+
+
+# def create_income_category(total_income: int):
+#     if total_income <= 100000:
+#         return 'ниже среднего'
+#     elif 100000 < total_income <= 200000:
+#         return 'средний'
+#     elif 200000 < total_income <= 300000:
+#         return 'выше среднего'
+#     elif 300000 < total_income <= 400000:
+#         return 'высокий'
+#     return 'очень высокий'
 #
-# for column in columns:
-#     if client_data.dtypes[column] == 'object':
-#         client_data[column] = client_data[column].str.lower()
 #
-# # Смена типов данных
-# client_data['total_income'] = client_data['total_income'].astype(int)
-#
-# # Удаление дубликатов
-# client_data = client_data.drop_duplicates().reset_index(drop=True)
-#
-#
-# # def get_lemma(row: str):
-# #     lemma = m.lemmatize(row)[-2]
-# #     return lemma
-#
-#
-# # def create_income_category(total_income: int):
-# #     if total_income <= 100000:
-# #         return 'ниже среднего'
-# #     elif 100000 < total_income <= 200000:
-# #         return 'средний'
-# #     elif 200000 < total_income <= 300000:
-# #         return 'выше среднего'
-# #     elif 300000 < total_income <= 400000:
-# #         return 'высокий'
-# #     return 'очень высокий'
-#
-#
-# # # создаю новый ДФ группировкой данных по цели кредита, считаю для каждой цели кол-во кредитов и количество невозвратов
-#
-# # debt_purpose = client_data
-# # debt_purpose.loc[(debt_purpose['purpose'] == 'покупка жилья для сдачи') | (debt_purpose['purpose'] == 'покупка жилья для семьи'), 'purpose'] = 'покупка жилья'
-# # debt_purpose['purpose'] = debt_purpose['purpose'].apply(get_lemma)
-# # debt_purpose.loc[debt_purpose['purpose'] == 'жилье', 'purpose'] = 'недвижимость'
-# # debt_purpose = debt_purpose.pivot_table(index='purpose', columns='debt', values='total_income', aggfunc='count', margins=True, margins_name='total_loans')
-# # debt_purpose.rename(columns={0: 'good_loans', 1: 'bad_loans'}, inplace=True)
-# # debt_purpose['fail_cr%'] = debt_purpose['bad_loans'] / debt_purpose['total_loans'] * 100
-# #
-# # print(debt_purpose)
+# создаю новый ДФ группировкой данных по цели кредита, считаю для каждой цели кол-во кредитов и количество невозвратов
+
+debt_purpose = client_data
+debt_purpose.loc[(debt_purpose['purpose'] == 'покупка жилья для сдачи') | (debt_purpose['purpose'] == 'покупка жилья для семьи'), 'purpose'] = 'покупка жилья'
+debt_purpose['purpose'] = debt_purpose['purpose'].apply(get_lemma)
+debt_purpose.loc[debt_purpose['purpose'] == 'жилье', 'purpose'] = 'недвижимость'
+# debt_purpose = debt_purpose.pivot_table(index='purpose', columns='debt', values='total_income', aggfunc='count', margins=True, margins_name='total_loans')
+# debt_purpose.rename(columns={0: 'good_loans', 1: 'bad_loans'}, inplace=True)
+# debt_purpose['fail_cr%'] = debt_purpose['bad_loans'] / debt_purpose['total_loans'] * 100
+
+print(debt_purpose[debt_purpose['income_type'] == 'пенсионер'].groupby('purpose')['total_income'].count())
+print(debt_purpose[debt_purpose['income_type'] == 'пенсионер'].groupby('dob_years')['total_income'].count())
 #
 # # purpose_slice = client_data.groupby('purpose', as_index=False).agg({'total_income': 'count', 'debt': 'sum'})
 # #
@@ -93,15 +94,23 @@ print(client_data.info())
 # # print()
 # # print(client_data)
 #
+
+
+# def add_child_category(row: str):
+#     if row == 0:
+#         return False
+#     return True
+#
+#
 # children = client_data
 # indexes_out = children[(children['children'] == 20) | (children['children'] == -1)].index
 # children.drop(indexes_out, inplace=True)
-# children.loc[children['children'] >= 3, 'children'] = '3+'
-# children = children.pivot_table(index='children', columns='debt', values='total_income', aggfunc='count', margins=True, margins_name='total_loans')
+# children['have_child'] = client_data['children'].apply(add_child_category)
+# children = children.pivot_table(index='have_child', columns='debt', values='total_income', aggfunc='count', margins=True, margins_name='total_loans')
 # children.rename(columns={0: 'good_loans', 1: 'bad_loans'}, inplace=True)
-# children['fail_cr%'] = round(children['bad_loans'] / children['total_loans'] * 100, 2)
+# children['fail_cr%'] = children['bad_loans'] / children['total_loans']
+# children.style.format({'fail_cr%': '{:.2%}'})
 # print(children)
-
 # for column in client_data.columns:
 #     print(column)
 #     print(client_data[column].value_counts(), end='\n\n')
