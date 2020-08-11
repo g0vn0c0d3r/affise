2f2c31f1309dd397aa1744f62154925937aa828b
leonovichmikhail <leonovich.mikhail@gmail.com>
2020-08-11 11:32:43 +0300

    1

--- Affise/main.py
+++ Affise/main.py
@@ -6,15 +6,17 @@ konga_cpa = 8
 lime_cpl = 15
 
 lime = Offer(offer_id=lime_cpa)
+konga = Offer(offer_id=konga_cpa)
 
-date_from = '2020-07-01'
+date_from = '2020-08-01'
 date_to = '2020-08-31'
 
 # konga = Offer(offer_id=konga_cpa)
-# konga.get_csv_reports(date_from=date_from, date_to=date_to, path='june/')
+# konga.get_csv_reports(date_from=date_from, date_to=date_to, path='reports/konga/july/')
 
-# rep = get_stats_by_day(offer_id=lime_cpa, date_from=date_from, date_to=date_to)
-# print(rep)
+print(get_stats_by_day(offer_id=lime_cpa, date_from=date_from, date_to=date_to))
+print()
+print(get_stats_by_day(offer_id=konga_cpa, date_from=date_from, date_to=date_to))
 # print()
 # partners_report = get_daily_stats_by_affiliate(offer_id=lime_cpa, date_from=date_from, date_to=date_to)
 # print(partners_report)
@@ -28,7 +30,7 @@ date_to = '2020-08-31'
 # print(rep2)
 # print()
 
-lime.get_csv_reports(date_from='2020-07-01', date_to='2020-07-31', path='reports/june')
+# lime.get_csv_reports(date_from='2020-07-01', date_to='2020-07-31', path='reports/june')
 
 # res = get_conversions_and_cost_by_webmaster(offer_id=lime_cpa, date_from=date_from, date_to=date_to, partner_id=30)
 # print(res)
