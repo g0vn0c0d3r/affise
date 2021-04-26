297f9d92bebd2afd1874c96305eb7d09b434dcb6
g0vn0c0d3r <leonovich.mikhail@gmail.com>
2021-04-26 11:31:43 +0300

    affise

--- Affise/sverka.py
+++ Affise/sverka.py
@@ -7,7 +7,7 @@ API_KEY = '0a3994e5f04ed3d755cba60eb50de7c6'
 date_from = '2021-03-01'
 date_to = '2021-03-31'
 status = 1
-limit = 3000
+limit = 2000
 
 resp = r.get(API_URL + '3.0/stats/conversions', headers={'API-Key': API_KEY},
              params=(
@@ -18,7 +18,6 @@ resp = r.get(API_URL + '3.0/stats/conversions', headers={'API-Key': API_KEY},
              ).json()
 
 pages = resp['pagination']['total_count'] // resp['pagination']['per_page'] + 1
-
 
 conversion_list = []
 for page in range(pages):
@@ -34,12 +33,14 @@ for page in range(pages):
         conversion_list.append(conversions)
 
 data = []
-columns = ['ts', 'project', 'offer_title', 'goal', 'payouts', 'sub1', 'sub2', 'sub3', 'sub4', 'sub5', 'sub6', 'affiliate']
+columns = ['ts', 'project', 'offer_title', 'goal_name', 'goal_id', 'payouts',
+           'sub1', 'sub2', 'sub3', 'sub4', 'sub5', 'sub6', 'affiliate']
 for conversion in conversion_list:
     ts = conversion['created_at']
     project = conversion['advertiser']['title']
     offer_title = conversion['offer']['title']
-    goal = conversion['goal']
+    goal_name = conversion['goal']
+    goal_id = conversion['action_id']
     payouts = conversion['payouts']
     sub1 = conversion['sub1']
     sub2 = conversion['sub2']
@@ -49,7 +50,7 @@ for conversion in conversion_list:
     sub6 = conversion['sub6']
     affiliate = conversion['partner']['title']
 
-    data.append([ts, project, offer_title, goal, payouts, sub1, sub2, sub3, sub4, sub5, sub6, affiliate])
+    data.append([ts, project, offer_title, goal_name, goal_id, payouts, sub1, sub2, sub3, sub4, sub5, sub6, affiliate])
 
 data_frame = pd.DataFrame(data=data, columns=columns)
 
