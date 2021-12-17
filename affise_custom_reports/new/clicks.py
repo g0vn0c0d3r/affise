d3bfa3e14f20e8c14610b760be86a628909ee6a4
mikhail <leonovich.mikhail@gmail.com>
2021-12-17 19:06:00 +0700

    SOME

--- affise_custom_reports/new/clicks.py
+++ affise_custom_reports/new/clicks.py
@@ -10,10 +10,11 @@ response = requests.get('https://api-lime-finance.affise.com/3.0/stats/custom?'
                         'slice[]=year'
                         '&slice[]=month'
                         '&slice[]=day'
-                        '&slice[]=affiliate'
-                        '&slice[]=sub3'
-                        '&filter[date_from]=2021-12-01'
-                        '&filter[date_to]=2021-12-16',
+                        # '&slice[]=affiliate'
+                        # '&slice[]=sub3'
+                        '&filter[date_from]=2021-01-01'
+                        '&filter[date_to]=2021-12-16'
+                        '&limit=500',
                         headers=headers).json()
 
 print(response)
